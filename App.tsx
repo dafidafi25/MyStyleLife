710adf290c2f5000452788cb43fc533cd703014e
Dafi <dafidafi25@gmail.com>
2023-01-12 13:29:00 +0700

    Setup Navigation Persistence

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import {AppNavigator} from '@navigators/AppNavigator';
+import {useNavigationPersistence} from '@navigators/NavigationUtilities';
 import React from 'react';
+import * as storage from '@utils/storage/index';
 
 interface IAppProps {}
 
+export const NAVIGATION_PERSISTENCE_KEY = 'NAVIGATION_STATE';
+
 export const App: React.FC<IAppProps> = () => {
-  return <AppNavigator />;
+  const {
+    initialNavigationState,
+    onNavigationStateChange,
+    isRestored: isNavigationStateRestored,
+  } = useNavigationPersistence(storage, NAVIGATION_PERSISTENCE_KEY);
+
+  if (!isNavigationStateRestored) return null;
+
+  return (
+    <AppNavigator
+      initialState={initialNavigationState}
+      onStateChange={onNavigationStateChange}
+    />
+  );
 };
 
 export default App;
