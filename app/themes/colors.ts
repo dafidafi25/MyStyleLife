40d05183a7366cd70c54ba15fcd48f7b4fcb0e82
Dafi <dafidafi25@gmail.com>
2023-01-12 12:02:45 +0700

    Add Colors Pallete

---
+++ app/themes/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  primary: '#E6ABAE',
+
+  secondary: '#0C866C',
+
+  Gray: '#737373',
+  Gray1: '#E0E0E0',
+
+  pink1: '#F0D7D4',
+  pink2: '#F8F4ED',
+
+  brown1: '#FFC22D',
+  brown2: '#764848',
+
+  white: '#ffffff',
+  background: '#E5E5E5',
+};
